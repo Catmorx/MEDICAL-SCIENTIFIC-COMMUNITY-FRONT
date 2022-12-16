335e0a07f2dd49da8cc6f78c6917128c5335caad
Catmorx <alismora3@gmail.com>
2022-12-16 15:39:52 -0500

    add view Agenda

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ import Login from "./views/Login";
 import Users from './views/Users';
 
 
-import {Link, Route, Routes} from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Register from "./views/Register";
+import Agenda from "./views/Agenda";
 
-export default function App(){
+export default function App() {
 
     //const [view, setView] = useState(<LoginClient></LoginClient>);
 
-   return(
-    <div>
-        {/* <Nav>
+    return (
+        <div>
+            {/* <Nav>
             <Button onClick={()=> setView(<Home></Home>)}>Home</Button>
             <Gap direction="vertical">1rem</Gap>
             <Button onClick={()=> setView(<Login></Login>)}>Login</Button>
@@ -25,28 +26,28 @@ export default function App(){
             <Button onClick={()=> setView(<User></User>)}>User</Button>
         </Nav>
         <Gap direction="vertical">1rem</Gap>
-          
+
         {view} */}
 
-        <nav className='flex'>
-            <Link to="/"><h2 className='topmenu'>HOME</h2></Link>
-            <Link to="/login"><h1 className='topmenu'>INICIAR</h1></Link>
-            <Link to="/register"><h1 className='topmenu'>REGISTRARSE</h1></Link>
-            <Link to="/user"><h1 className='topmenu'>USUARIOS</h1></Link>
-            
-        </nav>
+            <nav className='flex'>
+                <Link to="/"><h2 className='topmenu'>HOME</h2></Link>
+                <Link to="/login"><h1 className='topmenu'>INICIAR</h1></Link>
+                <Link to="/register"><h1 className='topmenu'>REGISTRARSE</h1></Link>
+                <Link to="/user"><h1 className='topmenu'>USUARIOS</h1></Link>
 
-        <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/login' element={<Login></Login>}></Route>
-            <Route path='/register' element={<Register></Register>}></Route>
-            <Route path='/user' element={<Users></Users>}></Route>
-            <Route path='/user/:name' element={<User></User>}></Route>
-            <Route path='*' element={<h1>404 NOT FOUND</h1>}></Route>
+            </nav>
 
-        </Routes>
-    </div> 
-   )
+            <Routes>
+                <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='/register' element={<Register></Register>}></Route>
+                <Route path='/user' element={<Users></Users>}></Route>
+                <Route path='/user/:name' element={<User></User>}></Route>
+                <Route path='*' element={<h1>404 NOT FOUND</h1>}></Route>
+                <Route path="/agenda" element={<Agenda></Agenda>}></Route>
+            </Routes>
+        </div>
+    )
 }
 
 
---
+++ src/views/Agenda.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import Input from '../components/forms/Input';
+import Gap from '../components/utils/Gap';
+import Button from '../components/forms/Button';
+import Card from '../components/utils/Card';
+
+export default function Agenda() {
+    const [fecha, setFecha] = useState("");
+    const [estado, setEstado] = useState("");
+    const [doctor, setDoctor] = useState("");
+    const [paciente, setPaciente] = useState("");
+    const [documento, setDocumento] = useState("");
+
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        alert("fecha: " + fecha + ", estado: " + estado + ", doctor: " + doctor + ", paciente: " + paciente + ", documento: " + documento);
+
+        await fetch("http://localhost:4000/agendar", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application.json",
+                fecha,
+                estado
+            }
+
+        })
+            .then(response => { return response.json() })
+            .catch(error => console.error(error))
+            .then(response => console.log(response));
+
+        await fetch("http://localhost:4000/doctor", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application.json",
+            }
+
+        })
+            .then(response => { return response.json() })
+            .catch(error => console.error(error))
+            .then(response => console.log(response));
+
+        fetch("http://localhost:4000/registro", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application.json",
+            }
+
+        })
+            .then(response => { return response.json() })
+            .catch(error => console.error(error))
+            .then(response => console.log(response[0].nombre));
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <Card padding="2rem" flexDirection="column" borderRadius="1rem">
+                <h1>Agenda</h1>
+                <Gap>2rem</Gap>
+                <Input type="Date" onChange={(v) => setFecha(v.target.value)}></Input>
+                <Gap>0.5rem</Gap>
+                <Input onChange={(v) => setDoctor(v.target.value)}>Nombre del Doctor</Input>
+                <Gap>0.5rem</Gap>
+                <Input onChange={(v) => setPaciente(v.target.value)}>Nombre del Paciente</Input>
+                <Gap>0.5rem</Gap>
+                <Input onChange={(v) => setDocumento(v.target.value)}>Documento</Input>
+                <Gap>0.5rem</Gap>
+                <Input onChange={(v) => setEstado(v.target.value)}>Estado</Input>
+                <Gap>2rem</Gap>
+                <Button type="submit"><b>AGENDAR</b></Button>
+            </Card>
+        </form>
+    )
+}
