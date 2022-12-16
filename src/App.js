3fa2775f9d5e976d112047bdf0326113d7451c03
JefferGonzalez <jefferd2016@gmail.com>
2022-12-16 16:10:28 -0500

    Se agrego la landing page con nav bar

--- src/App.js
+++ src/App.js
@@ -1,52 +1,29 @@
 /* import { useState } from "react";
 import LoginClient from "./views/LoginClient"
 import Button from "./components/forms/Button"; */
-import "./assets/global.css"
-import User from './views/User';
-import Home from './views/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import User from "./views/User";
+import { LandingPage } from "./views/LandingPage";
 import Login from "./views/Login";
-import Users from './views/Users';
-
-
-import {Link, Route, Routes} from "react-router-dom"
+import Users from "./views/Users";
 import Register from "./views/Register";
 
-export default function App(){
+import "./assets/global.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
-    //const [view, setView] = useState(<LoginClient></LoginClient>);
-
-   return(
-    <div>
-        {/* <Nav>
-            <Button onClick={()=> setView(<Home></Home>)}>Home</Button>
-            <Gap direction="vertical">1rem</Gap>
-            <Button onClick={()=> setView(<Login></Login>)}>Login</Button>
-            <Gap direction="vertical">1rem</Gap>
-            <Button onClick={()=> setView(<User></User>)}>User</Button>
-        </Nav>
-        <Gap direction="vertical">1rem</Gap>
-          
-        {view} */}
-
-        <nav className='flex'>
-            <Link to="/"><h2 className='topmenu'>HOME</h2></Link>
-            <Link to="/login"><h1 className='topmenu'>INICIAR</h1></Link>
-            <Link to="/register"><h1 className='topmenu'>REGISTRARSE</h1></Link>
-            <Link to="/user"><h1 className='topmenu'>USUARIOS</h1></Link>
-            
-        </nav>
-
-        <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/login' element={<Login></Login>}></Route>
-            <Route path='/register' element={<Register></Register>}></Route>
-            <Route path='/user' element={<Users></Users>}></Route>
-            <Route path='/user/:name' element={<User></User>}></Route>
-            <Route path='*' element={<h1>404 NOT FOUND</h1>}></Route>
-
-        </Routes>
-    </div> 
-   )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/user" element={<Users></Users>}></Route>
+        <Route path="/user/:name" element={<User></User>}></Route>
+        <Route path="*" element={<h1>404 NOT FOUND</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
