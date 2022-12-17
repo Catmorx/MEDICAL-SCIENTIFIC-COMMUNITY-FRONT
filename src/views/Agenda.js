b6dde9f7c3e23ddd662b4c91ee8e7319a9cbe012
Catmorx <alismora3@gmail.com>
2022-12-17 17:29:06 -0500

    Add Modal in Agenda

--- src/views/Agenda.js
+++ src/views/Agenda.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Input from '../components/forms/Input';
-import Gap from '../components/utils/Gap';
-import Button from '../components/forms/Button';
-import "../assets/Agenda.css"
-import Card from '../components/utils/Card';
-
+import Input from "../components/forms/Input";
+import Gap from "../components/utils/Gap";
+import Button from "../components/forms/Button";
+import "../assets/Agenda.css";
+import Card from "../components/utils/Card";
 
 export default function Agenda() {
     const [fecha, setFecha] = useState("");
@@ -14,10 +13,10 @@ export default function Agenda() {
     // const [paciente, setPaciente] = useState("");
     // const [documento, setDocumento] = useState("");
 
-
     async function onSubmit(e) {
-        e.preventDefault();
-        await fetch("http://localhost:4000/agendar", {
+        try {
+            e.preventDefault();
+            await fetch("http://localhost:4000/agendar", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -26,39 +25,77 @@ export default function Agenda() {
                 fecha,
                 estado,
                 usuario: "cat",
-                doctorName: doctor
-            })
-
+                doctorName: doctor,
+            }),
         })
-            .then(response => { return response.json() })
-            .catch(error => console.error(error))
-            .then(response => {
-                console.log(response);
-                e.target.reset()
+            .then((response) => {
+                return response.json();
+                
+            })
+            .catch((error) => {
+                alert("Llene los campos")
+                console.error(error)
+            })
+            .then((response) => {
+                alert("Cita Agendada")
+                e.target.reset();
             });
+        } catch (error) {
+        }
+        
+        
     }
 
     return (
         <>
-            <nav className='navbar navbar-expand-md nav-color py-0'>
-                <Link to="/"><h1 className='topmenu'>Home</h1></Link>
+            <nav className="navbar navbar-expand-md nav-color py-0">
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                >
+                    Pedir Cita
+                </button>
+                <Link to="/">
+                    <h1 className="topmenu">Home</h1>
+                </Link>
             </nav>
-            <form className='now' onSubmit={onSubmit}>
-                <Gap>2rem</Gap> 
-                <div class="position-relative">
-                    <div className="position-absolute bottom-25 end-50">
-                        <Gap>2rem</Gap>
-                        <h1>Agenda</h1>
-                        <Gap>2rem</Gap>
-                        <Input id="fecha" type="date" onChange={(v) => setFecha(v.target.value)}></Input>
-                        <Gap>0.5rem</Gap>
-                        <Input onChange={(v) => setDoctor(v.target.value)}>Nombre del Doctor</Input>
-                        <Gap>2rem</Gap>
-                        <Button type="submit"><b>AGENDAR</b></Button>
-                        <Gap>2rem</Gap>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Agendar Cita Medica</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form className='now' onSubmit={onSubmit}>
+                            <div className="modal-body">
+                                <Input id="fecha" type="date" onChange={(v) => setFecha(v.target.value)}></Input>
+                                <Gap>0.5rem</Gap>
+                                <Input onChange={(v) => setDoctor(v.target.value)}>Nombre del Doctor</Input>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" className="btn btn-primary">Agendar</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </form>
+            </div>
+            
+            <div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Hora</th>
+                            <th scope="col">Doctor</th>
+                            <th scope="col">Especialidad</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </>
-    )
+    );
 }
